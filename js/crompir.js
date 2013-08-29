90aaaf6785a612e7f942807a2bfbd508084b9f4b
Vladimir Ignatev <ya.na.pochte@gmail.com>
2013-08-29 22:29:52 +0200

    Merge branch 'master' of github.com:vladignatyev/crompir into ui
    Conflicts:
    	index.html

--- js/crompir.js
+++ js/crompir.js
@@ -34,7 +34,7 @@ crompir.resizeImage = function (srcImg, params) {
 
 crompir.processing = {
     brightness: function (r, g, b) { return 0.2126 * r + 0.7152 * g + 0.0722 * b; },
-    kernelFilter: function (imageData, width, height, kernel) {
+    kernelFilter: function (imageData, width, height, kernel, kernelSize, kernelCenter) {
         /*
         * [1 2 1
         *  2 4 2
@@ -53,17 +53,17 @@ crompir.processing = {
                 var Gt = 0;
                 var Bt = 0;
 
-                for (var kr = 0; kr < 3; kr++) {
-                    for (var kp = 0; kp < 3; kp++) {
-                        var pp = p + kp - 1; // -1 to center kernel
-                        var rr = r + kr - 1;
+                for (var kr = 0; kr < kernelSize; kr++) {
+                    for (var kp = 0; kp < kernelSize; kp++) {
+                        var pp = p + kp - kernelCenter; // -1 to center kernel
+                        var rr = r + kr - kernelCenter;
                         if (rr >= height) rr = height - 1;
                         else if (rr < 0) rr = 0;
                         if (pp >= width) pp = width - 1;
                         else if (pp < 0) pp = 0;
 
                         var index = (rr * width + pp) << 2; // *4
-                        var kindex = kr * 3 + kp;
+                        var kindex = kr * kernelSize + kp;
                         Rt += d[index] * kernel[kindex];
                         Gt += d[index + 1] * kernel[kindex];
                         Bt += d[index + 2] * kernel[kindex];
@@ -102,27 +102,33 @@ crompir.resizeImage2 = function (srcImg, params) {
         newHeight = srcImgHeight * params['scale'];
     } else {
         //fail with error
-        throw "Improper call to crompir.resizeImage";
+        throw "Improper call to crompir.resizeImage2";
     }
 
     var canvasCopy = document.createElement("canvas");
     var ctx = canvasCopy.getContext("2d");
-    canvasCopy.width = newWidth;
-    canvasCopy.height = newHeight;
-    ctx.drawImage(srcImg, 0, 0, newWidth, newHeight);
+    ctx.imageSmoothingEnabled = true;
+    canvasCopy.width = newWidth >> 0;
+    canvasCopy.height = newHeight >> 0;
+    ctx.drawImage(srcImg, 0, 0, newWidth >> 0, newHeight >> 0);
 
     // do antialiasing
-
+//
     d = ctx.getImageData(0,0, newWidth, newHeight);
 
     var s = 1;
 
     crompir.processing.kernelFilter(d, newWidth, newHeight,
-        [2, 1, 2,
-         1, 10, 1,
-         2, 1, 2]);
+        [
+            0.5, 0.5, 0.5,
+            0.5, 8.4,  0.5,
+            0.5, 0.5, 0.5
+        ], 3, 1);
+
 
     ctx.putImageData(d, 0, 0);
+
+
 
     return [canvasCopy, ctx];
 };
