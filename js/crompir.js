4effd0ca4f5cab4253bdd775c8c3d20d6d5ecc2c
Vladimir Ignatev <ya.na.pochte@gmail.com>
2013-08-30 00:52:20 +0200

    freeze

--- js/crompir.js
+++ js/crompir.js
@@ -33,18 +33,23 @@ crompir.resizeImage = function (srcImg, params) {
 };
 
 crompir.processing = {
-    brightness: function (r, g, b) { return 0.2126 * r + 0.7152 * g + 0.0722 * b; },
-    kernelFilter: function (imageData, width, height, kernel, kernelSize, kernelCenter) {
+    brightness: function (r, g, b) {
+        return 0.2126 * r + 0.7152 * g + 0.0722 * b;
+    },
+    kernelFilter: function (imageData, resultData, width, height, kernel, kernelSize, kernelCenter) {
         /*
-        * [1 2 1
-        *  2 4 2
-        *  1 2 1]
-        * */
+         * [1 2 1
+         *  2 4 2
+         *  1 2 1]
+         * */
 
-        var normalizeCoefficient = 0; var i=kernel.length; while(i--) normalizeCoefficient += kernel[i];
+        var normalizeCoefficient = 0;
+        var i = kernel.length;
+        while (i--) normalizeCoefficient += kernel[i];
         normalizeCoefficient = 1 / normalizeCoefficient;
 
-         var d = imageData.data;
+        var d = imageData.data;
+        var newd = resultData.data;
         for (var r = 0; r < height; r++) {
             for (var p = 0; p < width; p++) {
 
@@ -53,10 +58,12 @@ crompir.processing = {
                 var Gt = 0;
                 var Bt = 0;
 
+                var index2 = (r * width + p) << 2; //*4
+
                 for (var kr = 0; kr < kernelSize; kr++) {
                     for (var kp = 0; kp < kernelSize; kp++) {
-                        var pp = p + kp - kernelCenter; // -1 to center kernel
-                        var rr = r + kr - kernelCenter;
+                        var pp = p + kp - kernelCenter - 1; // -1 to center kernel
+                        var rr = r + kr - kernelCenter - 1;
                         if (rr >= height) rr = height - 1;
                         else if (rr < 0) rr = 0;
                         if (pp >= width) pp = width - 1;
@@ -64,6 +71,7 @@ crompir.processing = {
 
                         var index = (rr * width + pp) << 2; // *4
                         var kindex = kr * kernelSize + kp;
+
                         Rt += d[index] * kernel[kindex];
                         Gt += d[index + 1] * kernel[kindex];
                         Bt += d[index + 2] * kernel[kindex];
@@ -71,11 +79,10 @@ crompir.processing = {
                     }
                 }
 
-                var index2 = (r * width + p) << 2; //*4
-
-                d[index2]  = Rt * normalizeCoefficient;
-                d[index2+1] = Gt * normalizeCoefficient;
-                d[index2+2] = Bt * normalizeCoefficient;
+                newd[index2] = Rt * normalizeCoefficient;
+                newd[index2 + 1] = Gt * normalizeCoefficient;
+                newd[index2 + 2] = Bt * normalizeCoefficient;
+                newd[index2 + 3] = 255;
             }
         }
     }
@@ -105,29 +112,75 @@ crompir.resizeImage2 = function (srcImg, params) {
         throw "Improper call to crompir.resizeImage2";
     }
 
+    var tmpCanvas = document.createElement("canvas");
+    var tctx = tmpCanvas.getContext("2d");
+    tmpCanvas.width = srcImgWidth;
+    tmpCanvas.height = srcImgHeight;
+    tctx.drawImage(srcImg, 0, 0, srcImgWidth, srcImgHeight);
+
     var canvasCopy = document.createElement("canvas");
     var ctx = canvasCopy.getContext("2d");
-    ctx.imageSmoothingEnabled = true;
-    canvasCopy.width = newWidth >> 0;
-    canvasCopy.height = newHeight >> 0;
-    ctx.drawImage(srcImg, 0, 0, newWidth >> 0, newHeight >> 0);
+    canvasCopy.width = newWidth;
+    canvasCopy.height = newHeight;
+//    ctx.drawImage(srcImg, 0.002, 0.002, newWidth, newHeight);
 
     // do antialiasing
 //
-    d = ctx.getImageData(0,0, newWidth, newHeight);
+    d0 = tctx.getImageData(0,0,srcImgWidth, srcImgHeight );
 
-    var s = 1;
+    var factor = srcImgWidth / newWidth;
 
-    crompir.processing.kernelFilter(d, newWidth, newHeight,
-        [
-            0.5, 0.5, 0.5,
-            0.5, 8.4,  0.5,
-            0.5, 0.5, 0.5
-        ], 3, 1);
+    d = ctx.getImageData(0, 0, newWidth, newHeight);
+    for (var i = 0; i < newWidth; i++) {
+        for (var j = 0; j < newHeight; j++) {
+            var index = (j * newWidth + i) << 2; //*4
+            var r = 0.0;
+            var g = 0.0;
+            var b = 0.0;
+
+            var i0 = (i * factor);
+            var i1 = ((i+1)*factor) ;
+            var j0 = (j * factor) ;
+            var j1 = ((j+1)*factor) ;
+
+            for (var ii = i0; ii < i1; ii+=1) {
+             for (var ij = j0; ij < j1; ij+=1) {
+               var index2 = ((Math.floor(ij)) * srcImgWidth + ii >> 0) << 2; //*4
+                 r += d0.data[index2];
+                 g += d0.data[index2+1];
+                 b += d0.data[index2+2];
+            }
+            }
+            d.data[index] = r / factor / factor;
+            d.data[index+1] = g / factor / factor;
+            d.data[index+2] = b / factor / factor;
+            d.data[index+3] = 255;
+        }
+    }
+//
+//    var s = 1;
+
+//    var resultData = ctx.createImageData(newWidth, newHeight);
+
+//    crompir.processing.kernelFilter(d, resultData, newWidth, newHeight,
+////        [
+////            0.1,0.1,0.1,0.1,0.1,
+////            0.1,1.3,1.3,1.3,0.1,
+////            0.1,1.3,3.7,1.3,0.1,
+////            0.1,1.3,1.3,1.3,0.1,
+////            0.1,0.1,0.1,0.1,0.1
+////
+////        ]
+//        [
+//            1.3,1.3,1.3,
+//            1.3,3.7,1.3,
+//            1.3,1.3,1.3
+//
+//        ]
+//        , 3, 1);
 
 
     ctx.putImageData(d, 0, 0);
-
 
 
     return [canvasCopy, ctx];
