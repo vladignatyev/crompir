0027078409fc96e579658451f7408f23d0aa54a2
Nicht Nein <nchtnn@gmail.com>
2013-11-22 05:17:26 +0400

    замена меню, нерабочий блок правил ресайза и нерабочая кнопка сохранения одной фотки

--- js/ui.js
+++ js/ui.js
@@ -1,11 +1,20 @@
+$( document ).ready(function() {
+    $('#images').on('mouseenter', '.image_block', function() {
+        $(this).find('.image_save_button').removeClass('h');
+    });
+    $('#images').on('mouseleave', '.image_block', function() {
+        $(this).find('.image_save_button').addClass('h');
+    });
+    $('#images').on('click', '.image_save_button', function() {
+        alert('TODO сохранить данную картинку');
+    });
+});
+
 function createImage(image) {
     var div = $('<div class="image_block">');
     $('.images').append(div);
     $(div).append(image);
-}
-
-function closePopup() {
-    $('.popup_content,.popup_layout').remove();
+    $(div).append($('<div class="image_save_button h">').text('S'));
 }
 
 function setScale(value) {
